2a43a543fa6999267d4cc386a4686fd3d04e73c4
shivam <gulati.shivam2008@gmail.com>
2024-11-26 14:20:42 +0530

    Assignment done 1)First test cases has been created

--- src/main/java/basiscPrograming/FirstTestCase1.java
+++ src/main/java/basiscPrograming/FirstTestCase1.java
@@ -3,6 +3,61 @@ package basiscPrograming;
 public class FirstTestCase1 {
 
     public static void main(String[] args) {
-        System.out.println("First test case in day1");
+
+
+//// a+ b
+//        int age = 30 ;
+//        //  (Condition) ?  "Statement execute if condition true :Statement execute if condition false
+//        String Status = (age>=18)?"Adultrafer":"Minor";
+//
+//        System.out.println(Status);
+
+
+        // AND , OR
+        // 10001 AND 10001
+
+
+    int[][] myArray = new int[2][3];
+    /*
+
+       10 , 12 , 3
+       1 , 2,  4
+
+     */
+
+        myArray[0][0]=10;
+        myArray[0][1]=12;
+        myArray[0][2]=18;
+
+        myArray[1][0]=10;
+        myArray[1][1]=12;
+        myArray[1][2]=18;
+
+
+        // this is known as the array of the array
+
+//        int[][] myArray2 = {
+//                {1,2,3},{1,2,3}
+//
+//        };
+
+        //first row , col
+
+        /*
+
+        *
+        * *
+        * * *
+
+         */
+
+        for( int i= 0;i<myArray.length;i++){
+
+            for(int j=0;j<myArray[i].length;j++){
+                System.out.print(myArray[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
